0e100d79d201308a7254c00ac4bd885fcdcf62c9
Abdu <abdurahmann.benissa@ucalgary.ca>
2026-05-12 11:42:38 -0400

    add clsString with constructors, value property, and Length function

--- Vexstr/clsString.h
+++ Vexstr/clsString.h
@@ -1,8 +1,45 @@
 #pragma once
 #include <iostream>
 #include <vector>
+using namespace std;
+
 class clsString
 {
+private:
+    string _Value;
+
+public:
+
+    clsString()
+    {
+        _Value = "";
+    }
+
+    clsString(string Value)
+    {
+        _Value = Value;
+    }
+
+    void SetValue(string Value) {
+        _Value = Value;
+    }
+
+    string GetValue() {
+        return _Value;
+    }
+
+    __declspec(property(get = GetValue, put = SetValue)) string Value;
+
+
+    static short Length(string S1)
+    {
+        return S1.length();
+    };
+
+    short Length()
+    {
+        return _Value.length();
+    };
 
 };
 
